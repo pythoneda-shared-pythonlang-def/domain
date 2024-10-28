894114c04bca3c80a2402b134d9bc803c1848edd
rydnr <github@acm-sl.org>
2024-10-28 18:52:07 +0100

    Updated nixpkgs

--- flake.nix
+++ flake.nix
@@ -20,7 +20,7 @@
   description = "Support for event-driven architectures in Python";
   inputs = rec {
     flake-utils.url = "github:numtide/flake-utils/v1.0.0";
-    nixos.url = "github:NixOS/nixpkgs/23.11";
+    nixos.url = "github:NixOS/nixpkgs/24.05";
     pythoneda-shared-pythonlang-banner = {
       inputs.flake-utils.follows = "flake-utils";
       inputs.nixos.follows = "nixos";
