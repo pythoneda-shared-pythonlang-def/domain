0416a76f43ec6e7bab83e3ac4c3add52bf46e3b8
Jose San Leandro Armendáriz <github@acm-sl.org>
2024-10-31 19:31:40 +0100

    Commit created with sync-pythoneda.sh

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
     pythoneda-shared-pythonlang-banner = {
       inputs.flake-utils.follows = "flake-utils";
       inputs.nixos.follows = "nixos";
-      url = "github:pythoneda-shared-pythonlang-def/banner/0.0.53";
+      url = "github:pythoneda-shared-pythonlang-def/banner/0.0.56";
     };
   };
   outputs = inputs:
