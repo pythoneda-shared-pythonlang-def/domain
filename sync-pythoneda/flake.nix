6c005be0dbd3318a61ee4efab9c8c4bf17c8576c
rydnr <github@acm-sl.org>
2024-11-16 06:14:38 +0100

    Updated dependencies

--- sync-pythoneda/flake.nix
+++ sync-pythoneda/flake.nix
@@ -46,7 +46,7 @@
         org = "pythoneda-shared-pythonlang-def";
         repo = "domain";
         pname = "${org}-${repo}-sync-pythoneda";
-        version = "0.0.57";
+        version = "0.0.58";
         pkgs = import nixos { inherit system; };
         description =
           "dry-wit script to update PythonEDA projects' dependencies to their latest dependencies";
